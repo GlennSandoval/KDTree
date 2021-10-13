e1ca53906200977f2140e503c473db1b26181498
Glenn Sandoval <glenn.sandoval@gmail.com>
2021-10-13 13:41:47 +1300

    update code

--- KDTree/Tree.cs
+++ KDTree/Tree.cs
@@ -1,31 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 // My learning project for KD Trees
 
 namespace KDTree
 {
-
     public class Tree<T> where T : IComparable
     {
-
-        private Node<T> _root;
-        private const int MAXDEPTH = 10;
+        private const int MAX_DEPTH = 10;
         private const int MIN_NODE_ITEM_COUNT = 3;
 
         public Tree(IList<IList<T>> points)
         {
-            _root = new Node<T>();
-            _root.Data = points;
-            BuildTree(_root, 1);
+            var root = new Node<T>
+            {
+                Data = points
+            };
+            BuildTree(root, 1);
         }
 
         private void BuildTree(Node<T> node, int depth)
         {
-            if (depth >= MAXDEPTH || node.Data.Count <= MIN_NODE_ITEM_COUNT)
+            if (depth >= MAX_DEPTH || node.Data.Count <= MIN_NODE_ITEM_COUNT)
             {
                 return;
             }
@@ -42,7 +38,6 @@ namespace KDTree
 
             var median = data[(int)Math.Floor(data.Count / 2.0)];
 
-            var splitPos = data.IndexOf(median);
             var leftNode = new Node<T>();
             node.Left = leftNode;
 
@@ -63,10 +58,7 @@ namespace KDTree
 
             BuildTree(leftNode, depth + 1);
             BuildTree(rightNode, depth + 1);
-
         }
-
-
     }
 
     class Node<T> where T : IComparable
@@ -75,5 +67,4 @@ namespace KDTree
         public Node<T> Right;
         public IList<IList<T>> Data = new List<IList<T>>();
     }
-
 }
